7655bfc6fbabb9c97521cf46ddfa064c31874644
Xavier Perez <duosrx@gmail.com>
2019-07-16 00:44:20 -0700

    Massive fixes

--- lib/cpu.ml
+++ lib/cpu.ml
@@ -37,23 +37,21 @@ type cpu = {
   memory : int array;
 }
 
-let load_byte cpu address = cpu.memory.(address)
-let load_next_byte cpu = load_byte cpu cpu.pc
+let load_byte cpu address =
+  cpu.memory.(address)
+
+let store_byte cpu address value =
+  cpu.memory.(address) <- value
 
 let load_word cpu address =
   let a = cpu.memory.(address) in
   let b = cpu.memory.(address + 1) in
   a lor b lsl 8
 
-let load_next_word cpu = load_word cpu cpu.pc
-
 let load_word_zero_page cpu address =
   let a = cpu.memory.(address) in
   let b = cpu.memory.(wrapping_add address 1) in
   a lor b lsl 8
-
-let store_byte cpu address value =
-  cpu.memory.(address) <- value
 
 let store_word cpu address value =
   let lo = value land 0xFF in
@@ -100,7 +98,7 @@ end
 
 open AddressingMode
 
-let decode_addressing_mode cpu am =
+let decode_addressing_mode cpu am extra_page_cycles =
   let pc = cpu.pc + 1 in
   match am with
   | Immediate ->
@@ -111,12 +109,12 @@ let decode_addressing_mode cpu am =
   | AbsoluteX ->
     let arg = load_word cpu pc in
     let address = wrapping_add_w arg cpu.x in
-    if page_crossed arg address then cpu.extra_cycles <- cpu.extra_cycles + 1;
+    if page_crossed arg address then cpu.extra_cycles <- cpu.extra_cycles + extra_page_cycles;
     (load_byte cpu address, Some address, 2)
   | AbsoluteY ->
     let arg = load_word cpu pc in
     let address = wrapping_add_w arg cpu.y in
-    if page_crossed arg address then cpu.extra_cycles <- cpu.extra_cycles + 1;
+    if page_crossed arg address then cpu.extra_cycles <- cpu.extra_cycles + extra_page_cycles;
     (load_byte cpu address, Some address, 2)
   | ZeroPage ->
     let address = load_byte cpu pc in
@@ -128,7 +126,10 @@ let decode_addressing_mode cpu am =
     let address = wrapping_add (load_byte cpu pc) cpu.y in
     (load_byte cpu address, Some address, 1)
   | Relative ->
-    (load_byte cpu pc, Some pc, 1)
+    let byte = load_byte cpu pc in
+    let offset = if byte < 0x80 then cpu.pc + byte else cpu.pc + byte - 0x100 in
+    (offset, Some pc, 1)
+    (* (load_byte cpu pc, Some pc, 1) *)
   | IndirectX ->
     let zero_page = wrapping_add (load_byte cpu pc) cpu.x in
     let address = load_word_zero_page cpu zero_page in
@@ -137,7 +138,7 @@ let decode_addressing_mode cpu am =
     let zero_page = load_byte cpu pc in
     let word = load_word_zero_page cpu zero_page in
     let address = wrapping_add_w word cpu.y in
-    if page_crossed word address then cpu.extra_cycles <- cpu.extra_cycles + 1;
+    if page_crossed word address then cpu.extra_cycles <- cpu.extra_cycles + extra_page_cycles;
     (load_byte cpu address, Some(address), 1)
   | Indirect ->
     let address = load_word cpu pc in
@@ -206,14 +207,12 @@ let brk _ = failwith "oh no BRK"
   cpu.interrupt <- true;
   cpu.pc <- load_word cpu 0xFFEE *)
 
-let branch cpu offset cond =
+let branch cpu addr cond =
   if cond then
-    (* Treat offset as a 8 bit signed integer and wrap around if necessary *)
-    let c = (cpu.pc + offset) land 0xFFFF in
-    (* Add an extra cycle when cond is true and one more when crossing a page boundary *)
-    let cycles = if page_crossed c (cpu.pc + 2) then 2 else 1 in
+    let cycles = if page_crossed addr (cpu.pc + 2) then 2 else 1 in
     cpu.extra_cycles <- cpu.extra_cycles + cycles;
-    cpu.pc <- c
+    cpu.pc <- addr
+
 
 let bcs cpu offset = branch cpu offset cpu.carry
 let bcc cpu offset = branch cpu offset (not cpu.carry)
@@ -338,10 +337,10 @@ let args_to_string cpu i =
     let sum = wrapping_sub_w (Option.value_exn i.target) cpu.y in
     sprintf "($%02X),Y = %04X @ %04X = %02X" byte sum (Option.value_exn i.target) i.args
   | Indirect -> sprintf "($%04X) = %04X" (load_word cpu (cpu.pc + 1)) (Option.value_exn i.target)
-  | Relative -> sprintf "$%04X" ((Option.value_exn i.target) + i.args + 1)
+  | Relative ->
+    sprintf "$%04X" (i.args + 2);
   | Accumulator -> "A"
   | Implicit -> ""
-  (* | _ -> failwith @@ sprintf "can't print %s" @@ show_instr i *)
 
 let word_to_byte_string w =
   let lo = w land 0xFF in
@@ -350,8 +349,8 @@ let word_to_byte_string w =
 
 let args_to_hex_string cpu i =
   match i.mode with
-  | Immediate | Relative -> sprintf "%02X" i.args
-  | IndirectX | IndirectY | ZeroPageX | ZeroPageY -> sprintf "%02X" (load_byte cpu (cpu.pc + 1))
+  | Immediate -> sprintf "%02X" i.args
+  | IndirectX | IndirectY | ZeroPageX | ZeroPageY | Relative -> sprintf "%02X" (load_byte cpu (cpu.pc + 1))
   | Absolute -> word_to_byte_string (Option.value_exn i.target)
   | Indirect | AbsoluteX | AbsoluteY -> word_to_byte_string (load_word cpu (cpu.pc + 1))
   | ZeroPage -> sprintf "%02X" (Option.value_exn i.target)
@@ -359,154 +358,167 @@ let args_to_hex_string cpu i =
 
 let decode opcode =
   match opcode with
-  | 0x00 -> (BRK, Implicit, 7)
-  | 0x01 -> (ORA, IndirectX, 6)
-  | 0x05 -> (ORA, ZeroPage, 3)
-  | 0x06 -> (ASL, ZeroPage, 5)
-  | 0x08 -> (PHP, Implicit, 3)
-  | 0x09 -> (ORA, Immediate, 2)
-  | 0x0A -> (ASL, Accumulator, 2)
-  | 0x0D -> (ORA, Absolute, 4)
-  | 0x0E -> (ASL, Absolute, 6)
-  | 0x10 -> (BPL, Relative, 2)
-  | 0x11 -> (ORA, IndirectY, 5)
-  | 0x15 -> (ORA, ZeroPageX, 4)
-  | 0x16 -> (ASL, ZeroPageX, 6)
-  | 0x18 -> (CLC, Implicit, 2)
-  | 0x19 -> (ORA, AbsoluteY, 4)
-  | 0x1D -> (ORA, AbsoluteX, 4)
-  | 0x20 -> (JSR, Absolute, 6)
-  | 0x21 -> (AND, IndirectX, 6)
-  | 0x24 -> (BIT, ZeroPage, 3)
-  | 0x25 -> (AND, ZeroPage, 3)
-  | 0x26 -> (ROL, ZeroPage, 5)
-  | 0x28 -> (PLP, Implicit, 4)
-  | 0x29 -> (AND, Immediate, 2)
-  | 0x2A -> (ROL, Accumulator, 2)
-  | 0x2D -> (AND, Absolute, 4)
-  | 0x2C -> (BIT, Absolute, 4)
-  | 0x2E -> (ROL, Absolute, 6)
-  | 0x30 -> (BMI, Relative, 2)
-  | 0x31 -> (AND, IndirectY, 5)
-  | 0x35 -> (AND, ZeroPageX, 4)
-  | 0x36 -> (ROL, ZeroPageX, 6)
-  | 0x38 -> (SEC, Implicit, 2)
-  | 0x39 -> (AND, AbsoluteY, 4)
-  | 0x3D -> (AND, AbsoluteX, 4)
-  | 0x40 -> (RTI, Implicit, 6)
-  | 0x41 -> (EOR, IndirectX, 6)
-  | 0x45 -> (EOR, ZeroPage, 3)
-  | 0x46 -> (LSR, ZeroPage, 5)
-  | 0x48 -> (PHA, Implicit, 3)
-  | 0x49 -> (EOR, Immediate, 2)
-  | 0x4A -> (LSR, Accumulator, 2)
-  | 0x4C -> (JMP, Absolute, 3)
-  | 0x4D -> (EOR, Absolute, 4)
-  | 0x4E -> (LSR, Absolute, 6)
-  | 0x50 -> (BVC, Relative, 2)
-  | 0x51 -> (EOR, IndirectY, 5)
-  | 0x55 -> (EOR, ZeroPageX, 4)
-  | 0x56 -> (LSR, ZeroPageX, 6)
-  | 0x59 -> (EOR, AbsoluteY, 4)
-  | 0x5D -> (EOR, AbsoluteX, 4)
-  | 0x60 -> (RTS, Implicit, 6)
-  | 0x61 -> (ADC, IndirectX, 6)
-  | 0x65 -> (ADC, ZeroPage, 3)
-  | 0x66 -> (ROR, ZeroPage, 5)
-  | 0x68 -> (PLA, Implicit, 4)
-  | 0x69 -> (ADC, Immediate, 2)
-  | 0x6A -> (ROR, Accumulator, 2)
-  | 0x6C -> (JMP, Indirect, 5)
-  | 0x6D -> (ADC, Absolute, 4)
-  | 0x6E -> (ROR, Absolute, 6)
-  | 0x70 -> (BVS, Relative, 2)
-  | 0x71 -> (ADC, IndirectY, 5)
-  | 0x75 -> (ADC, ZeroPageX, 4)
-  | 0x76 -> (ROR, ZeroPageX, 6)
-  | 0x78 -> (SEI, Implicit, 2)
-  | 0x79 -> (ADC, AbsoluteY, 4)
-  | 0x7D -> (ADC, AbsoluteX, 4)
-  | 0x81 -> (STA, IndirectX, 6)
-  | 0x84 -> (STY, ZeroPage, 3)
-  | 0x85 -> (STA, ZeroPage, 3)
-  | 0x86 -> (STX, ZeroPage, 3)
-  | 0x88 -> (DEY, Implicit, 2)
-  | 0x8A -> (TXA, Implicit, 2)
-  | 0x8C -> (STY, Absolute, 4)
-  | 0x8D -> (STA, Absolute, 4)
-  | 0x8E -> (STX, Absolute, 4)
-  | 0x90 -> (BCC, Relative, 2)
-  | 0x91 -> (STA, IndirectY, 6)
-  | 0x94 -> (STY, ZeroPageX, 4)
-  | 0x95 -> (STA, ZeroPageX, 4)
-  | 0x9A -> (TXS, Implicit, 2)
-  | 0x96 -> (STX, ZeroPageY, 4)
-  | 0x98 -> (TYA, Implicit, 2)
-  | 0x99 -> (STA, AbsoluteY, 5)
-  | 0x9D -> (STA, AbsoluteX, 5)
-  | 0xA0 -> (LDY, Immediate, 2)
-  | 0xA1 -> (LDA, IndirectX, 6)
-  | 0xA2 -> (LDX, Immediate, 2)
-  | 0xA4 -> (LDY, ZeroPage, 3)
-  | 0xA5 -> (LDA, ZeroPage, 3)
-  | 0xA6 -> (LDX, ZeroPage, 3)
-  | 0xA9 -> (LDA, Immediate, 2)
-  | 0xA8 -> (TAY, Implicit, 2)
-  | 0xAA -> (TAX, Implicit, 2)
-  | 0xAC -> (LDY, Absolute, 4)
-  | 0xAD -> (LDA, Absolute, 4)
-  | 0xAE -> (LDX, Absolute, 4)
-  | 0xB0 -> (BCS, Relative, 2)
-  | 0xB1 -> (LDA, IndirectY, 5)
-  | 0xB4 -> (LDY, ZeroPageX, 4)
-  | 0xB5 -> (LDA, ZeroPageX, 4)
-  | 0xB6 -> (LDX, ZeroPageY, 4)
-  | 0xB8 -> (CLV, Implicit, 2)
-  | 0xB9 -> (LDA, AbsoluteY, 4)
-  | 0xBA -> (TSX, Implicit, 2)
-  | 0xBD -> (LDA, AbsoluteX, 4)
-  | 0xBC -> (LDY, AbsoluteX, 4)
-  | 0xC0 -> (CPY, Immediate, 2)
-  | 0xC1 -> (CMP, IndirectX, 6)
-  | 0xC4 -> (CPY, ZeroPage, 3)
-  | 0xC5 -> (CMP, ZeroPage, 3)
-  | 0xC6 -> (DEC, ZeroPage, 5)
-  | 0xC8 -> (INY, Implicit, 2)
-  | 0xC9 -> (CMP, Immediate, 2)
-  | 0xCA -> (DEX, Implicit, 2)
-  | 0xCC -> (CPY, Absolute, 4)
-  | 0xCD -> (CMP, Absolute, 4)
-  | 0xCE -> (DEC, Absolute, 6)
-  | 0xD0 -> (BNE, Relative, 2)
-  | 0xD1 -> (CMP, IndirectY, 5)
-  | 0xD5 -> (CMP, ZeroPageX, 4)
-  | 0xD6 -> (DEC, ZeroPageX, 6)
-  | 0xD8 -> (CLD, Implicit, 2)
-  | 0xD9 -> (CMP, AbsoluteY, 4)
-  | 0xDD -> (CMP, AbsoluteX, 4)
-  | 0xE0 -> (CPX, Immediate, 2)
-  | 0xE1 -> (SBC, IndirectX, 6)
-  | 0xE4 -> (CPX, ZeroPage, 3)
-  | 0xE5 -> (SBC, ZeroPage, 3)
-  | 0xE6 -> (INC, ZeroPage, 5)
-  | 0xE8 -> (INX, Implicit, 2)
-  | 0xE9 -> (SBC, Immediate, 2)
-  | 0xEA -> (NOP, Implicit, 2)
-  | 0xEC -> (CPX, Absolute, 4)
-  | 0xED -> (SBC, Absolute, 4)
-  | 0xEE -> (INC, Absolute, 6)
-  | 0xF0 -> (BEQ, Relative, 2)
-  | 0xF1 -> (SBC, IndirectY, 5)
-  | 0xF5 -> (SBC, ZeroPageX, 4)
-  | 0xF6 -> (INC, ZeroPageX, 6)
-  | 0xF8 -> (SED, Implicit, 2)
-  | 0xF9 -> (SBC, AbsoluteY, 4)
-  | 0xFD -> (SBC, AbsoluteX, 4)
+  | 0x1A | 0x3A | 0x5A | 0x7A | 0xDA | 0xFA -> (NOP, Implicit, 2, 0)
+  | 0x80 | 0x82 | 0x89 | 0xC2 | 0xE2 -> (NOP, Immediate, 2, 0)
+  | 0x04 | 0x44 | 0x64 -> (NOP, ZeroPage, 3, 0)
+  | 0x0C -> (NOP, Absolute, 4, 0)
+  | 0x14 | 0x34 | 0x54 | 0x74 | 0xD4 | 0xF4 -> (NOP, ZeroPageX, 4, 0)
+  | 0x1C | 0x3C | 0x5C | 0x7C | 0xDC | 0xFC -> (NOP, AbsoluteX, 4, 1)
+  | 0x00 -> (BRK, Implicit, 7, 0)
+  | 0x01 -> (ORA, IndirectX, 6, 0)
+  | 0x05 -> (ORA, ZeroPage, 3, 0)
+  | 0x06 -> (ASL, ZeroPage, 5, 0)
+  | 0x08 -> (PHP, Implicit, 3, 0)
+  | 0x09 -> (ORA, Immediate, 2, 0)
+  | 0x0A -> (ASL, Accumulator, 2, 0)
+  | 0x0D -> (ORA, Absolute, 4, 0)
+  | 0x0E -> (ASL, Absolute, 6, 0)
+  | 0x10 -> (BPL, Relative, 2, 0)
+  | 0x11 -> (ORA, IndirectY, 5, 0)
+  | 0x15 -> (ORA, ZeroPageX, 4, 0)
+  | 0x16 -> (ASL, ZeroPageX, 6, 0)
+  | 0x18 -> (CLC, Implicit, 2, 0)
+  | 0x19 -> (ORA, AbsoluteY, 4, 0)
+  | 0x1D -> (ORA, AbsoluteX, 4, 0)
+  | 0x1E -> (ASL, AbsoluteX, 7, 0)
+  | 0x20 -> (JSR, Absolute, 6, 0)
+  | 0x21 -> (AND, IndirectX, 6, 0)
+  | 0x24 -> (BIT, ZeroPage, 3, 0)
+  | 0x25 -> (AND, ZeroPage, 3, 0)
+  | 0x26 -> (ROL, ZeroPage, 5, 0)
+  | 0x28 -> (PLP, Implicit, 4, 0)
+  | 0x29 -> (AND, Immediate, 2, 0)
+  | 0x2A -> (ROL, Accumulator, 2, 0)
+  | 0x2D -> (AND, Absolute, 4, 0)
+  | 0x2C -> (BIT, Absolute, 4, 0)
+  | 0x2E -> (ROL, Absolute, 6, 0)
+  | 0x30 -> (BMI, Relative, 2, 1)
+  | 0x31 -> (AND, IndirectY, 5, 0)
+  | 0x35 -> (AND, ZeroPageX, 4, 0)
+  | 0x36 -> (ROL, ZeroPageX, 6, 0)
+  | 0x38 -> (SEC, Implicit, 2, 0)
+  | 0x39 -> (AND, AbsoluteY, 4, 0)
+  | 0x3D -> (AND, AbsoluteX, 4, 0)
+  | 0x3E -> (ROL, AbsoluteX, 7, 0)
+  | 0x40 -> (RTI, Implicit, 6, 0)
+  | 0x41 -> (EOR, IndirectX, 6, 0)
+  | 0x45 -> (EOR, ZeroPage, 3, 0)
+  | 0x46 -> (LSR, ZeroPage, 5, 0)
+  | 0x48 -> (PHA, Implicit, 3, 0)
+  | 0x49 -> (EOR, Immediate, 2, 0)
+  | 0x4A -> (LSR, Accumulator, 2, 0)
+  | 0x4C -> (JMP, Absolute, 3, 0)
+  | 0x4D -> (EOR, Absolute, 4, 0)
+  | 0x4E -> (LSR, Absolute, 6, 0)
+  | 0x50 -> (BVC, Relative, 2, 1)
+  | 0x51 -> (EOR, IndirectY, 5, 0)
+  | 0x55 -> (EOR, ZeroPageX, 4, 0)
+  | 0x56 -> (LSR, ZeroPageX, 6, 0)
+  | 0x59 -> (EOR, AbsoluteY, 4, 0)
+  | 0x5D -> (EOR, AbsoluteX, 4, 0)
+  | 0x5E -> (LSR, AbsoluteX, 7, 0)
+  | 0x60 -> (RTS, Implicit, 6, 0)
+  | 0x61 -> (ADC, IndirectX, 6, 0)
+  | 0x65 -> (ADC, ZeroPage, 3, 0)
+  | 0x66 -> (ROR, ZeroPage, 5, 0)
+  | 0x68 -> (PLA, Implicit, 4, 0)
+  | 0x69 -> (ADC, Immediate, 2, 0)
+  | 0x6A -> (ROR, Accumulator, 2, 0)
+  | 0x6C -> (JMP, Indirect, 5, 0)
+  | 0x6D -> (ADC, Absolute, 4, 0)
+  | 0x6E -> (ROR, Absolute, 6, 0)
+  | 0x70 -> (BVS, Relative, 2, 1)
+  | 0x71 -> (ADC, IndirectY, 5, 0)
+  | 0x75 -> (ADC, ZeroPageX, 4, 0)
+  | 0x76 -> (ROR, ZeroPageX, 6, 0)
+  | 0x78 -> (SEI, Implicit, 2, 0)
+  | 0x79 -> (ADC, AbsoluteY, 4, 0)
+  | 0x7D -> (ADC, AbsoluteX, 4, 0)
+  | 0x7E -> (ROR, AbsoluteX, 7, 0)
+  | 0x81 -> (STA, IndirectX, 6, 0)
+  | 0x84 -> (STY, ZeroPage, 3, 0)
+  | 0x85 -> (STA, ZeroPage, 3, 0)
+  | 0x86 -> (STX, ZeroPage, 3, 0)
+  | 0x88 -> (DEY, Implicit, 2, 0)
+  | 0x8A -> (TXA, Implicit, 2, 0)
+  | 0x8C -> (STY, Absolute, 4, 0)
+  | 0x8D -> (STA, Absolute, 4, 0)
+  | 0x8E -> (STX, Absolute, 4, 0)
+  | 0x90 -> (BCC, Relative, 2, 0)
+  | 0x91 -> (STA, IndirectY, 6, 0)
+  | 0x94 -> (STY, ZeroPageX, 4, 0)
+  | 0x95 -> (STA, ZeroPageX, 4, 0)
+  | 0x9A -> (TXS, Implicit, 2, 0)
+  | 0x96 -> (STX, ZeroPageY, 4, 0)
+  | 0x98 -> (TYA, Implicit, 2, 0)
+  | 0x99 -> (STA, AbsoluteY, 5, 0)
+  | 0x9D -> (STA, AbsoluteX, 5, 0)
+  | 0xA0 -> (LDY, Immediate, 2, 0)
+  | 0xA1 -> (LDA, IndirectX, 6, 0)
+  | 0xA2 -> (LDX, Immediate, 2, 0)
+  | 0xA4 -> (LDY, ZeroPage, 3, 0)
+  | 0xA5 -> (LDA, ZeroPage, 3, 0)
+  | 0xA6 -> (LDX, ZeroPage, 3, 0)
+  | 0xA9 -> (LDA, Immediate, 2, 0)
+  | 0xA8 -> (TAY, Implicit, 2, 0)
+  | 0xAA -> (TAX, Implicit, 2, 0)
+  | 0xAC -> (LDY, Absolute, 4, 0)
+  | 0xAD -> (LDA, Absolute, 4, 0)
+  | 0xAE -> (LDX, Absolute, 4, 0)
+  | 0xB0 -> (BCS, Relative, 2, 1)
+  | 0xB1 -> (LDA, IndirectY, 5, 1)
+  | 0xB4 -> (LDY, ZeroPageX, 4, 0)
+  | 0xB5 -> (LDA, ZeroPageX, 4, 0)
+  | 0xB6 -> (LDX, ZeroPageY, 4, 0)
+  | 0xB8 -> (CLV, Implicit, 2, 0)
+  | 0xB9 -> (LDA, AbsoluteY, 4, 1)
+  | 0xBA -> (TSX, Implicit, 2, 0)
+  | 0xBD -> (LDA, AbsoluteX, 4, 1)
+  | 0xBC -> (LDY, AbsoluteX, 4, 1)
+  | 0xBE -> (LDX, AbsoluteY, 4, 1)
+  | 0xC0 -> (CPY, Immediate, 2, 0)
+  | 0xC1 -> (CMP, IndirectX, 6, 0)
+  | 0xC4 -> (CPY, ZeroPage, 3, 0)
+  | 0xC5 -> (CMP, ZeroPage, 3, 0)
+  | 0xC6 -> (DEC, ZeroPage, 5, 0)
+  | 0xC8 -> (INY, Implicit, 2, 0)
+  | 0xC9 -> (CMP, Immediate, 2, 0)
+  | 0xCA -> (DEX, Implicit, 2, 0)
+  | 0xCC -> (CPY, Absolute, 4, 0)
+  | 0xCD -> (CMP, Absolute, 4, 0)
+  | 0xCE -> (DEC, Absolute, 6, 0)
+  | 0xD0 -> (BNE, Relative, 2, 0)
+  | 0xD1 -> (CMP, IndirectY, 5, 0)
+  | 0xD5 -> (CMP, ZeroPageX, 4, 0)
+  | 0xD6 -> (DEC, ZeroPageX, 6, 0)
+  | 0xD8 -> (CLD, Implicit, 2, 0)
+  | 0xD9 -> (CMP, AbsoluteY, 4, 0)
+  | 0xDD -> (CMP, AbsoluteX, 4, 0)
+  | 0xDE -> (DEC, AbsoluteX, 7, 0)
+  | 0xE0 -> (CPX, Immediate, 2, 0)
+  | 0xE1 -> (SBC, IndirectX, 6, 0)
+  | 0xE4 -> (CPX, ZeroPage, 3, 0)
+  | 0xE5 -> (SBC, ZeroPage, 3, 0)
+  | 0xE6 -> (INC, ZeroPage, 5, 0)
+  | 0xE8 -> (INX, Implicit, 2, 0)
+  | 0xE9 -> (SBC, Immediate, 2, 0)
+  | 0xEA -> (NOP, Implicit, 2, 0)
+  | 0xEC -> (CPX, Absolute, 4, 0)
+  | 0xED -> (SBC, Absolute, 4, 0)
+  | 0xEE -> (INC, Absolute, 6, 0)
+  | 0xF0 -> (BEQ, Relative, 2, 1)
+  | 0xF1 -> (SBC, IndirectY, 5, 0)
+  | 0xF5 -> (SBC, ZeroPageX, 4, 0)
+  | 0xF6 -> (INC, ZeroPageX, 6, 0)
+  | 0xF8 -> (SED, Implicit, 2, 0)
+  | 0xF9 -> (SBC, AbsoluteY, 4, 0)
+  | 0xFD -> (SBC, AbsoluteX, 4, 0)
+  | 0xFE -> (INC, AbsoluteX, 7, 0)
   | _ -> failwith @@ sprintf "Unknown opcode %#02x" opcode
 
 let decode_instruction cpu instruction =
-  let (op, mode, cycles) = decode instruction in
-  let (args, target, size) = decode_addressing_mode cpu mode in
+  let (op, mode, cycles, extra_page_cycles) = decode instruction in
+  let (args, target, size) = decode_addressing_mode cpu mode extra_page_cycles in
   { op; mode; cycles; args; target; size = size + 1 }
 
 let execute_instruction cpu instruction =
