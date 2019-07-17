e6b272241605ab99db8fa781fc3468293e0876f7
Xavier Perez <duosrx@gmail.com>
2019-07-17 11:22:10 -0700

    More massive changes

--- bin/main.ml
+++ bin/main.ml
@@ -3,9 +3,16 @@ open Nes
 open Nes.Cpu
 open Nes.Cartridge
 
-let log_length = 10
+let run_length = 10000000
+let log_length = 60
 let logs = Array.create ~len:log_length ""
 let steps = ref 0
+let term = ref false
+
+let rec n_times f n =
+  if n > 0 then (
+    f (); n_times f (n - 1)
+  )
 
 let trace (cpu : cpu) instruction opcode =
   let cy = cpu.cycles * 3 mod 341 in
@@ -13,27 +20,40 @@ let trace (cpu : cpu) instruction opcode =
   let status = sprintf "A:%02X X:%02X Y:%02X P:%02X SP:%02X CYC:%3d" cpu.a cpu.x cpu.y (flags_to_int cpu) cpu.s cy in
   let str_op = Instructions.show_instruction instruction.op in
   let instr = sprintf "%s %s" str_op (args_to_string cpu instruction) in
+  (* let instr = str_op in *)
   let log = sprintf "%04X  %02X %-6s%-32s %s" cpu.pc opcode args instr status in
   logs.(!steps % log_length) <- log
 
 let main () =
   let memory = Array.create ~len:0x10000 0 in
   let rom = load_rom "./donkey.nes" in
+
   let cpu = {
-    rom = rom; ppu = Ppu.make (); cycles = 0;
+    rom = rom; ppu = Ppu.make ~rom; cycles = 0;
     a = 0; x = 0; y = 0; memory; s = 0xFD; pc = 0; extra_cycles = 0;
-    zero = false; negative = false; carry = false; decimal = false; interrupt = true; overflow = false
+    zero = false; negative = false; carry = false; decimal = false; interrupt = true; overflow = false;
+    nestest = false
   } in
 
   cpu.pc <- Cpu.load_word cpu 0xFFFC;
 
-  let term = ref false in
-
-  while !steps < 1000 && not !term do
+  while !steps < run_length && not !term do
+    let cycles = cpu.cycles in
     step cpu ~trace_fun:trace;
+    (* try step cpu ~trace_fun:trace
+    with Failure f -> (
+      print_endline f;
+      Array.iter logs ~f:print_endline;
+    ); *)
+
+    let elapsed = cpu.cycles - cycles in
+    n_times (fun () -> Ppu.step cpu.ppu) (elapsed * 3);
+    if cpu.ppu.nmi then Cpu.nmi cpu;
     print_endline logs.(!steps % log_length);
+    steps := !steps + 1
   done;
 
-  Array.iter logs ~f:print_endline
+  Array.iter logs ~f:print_endline;
+  ()
 
 let () = main ()
--- lib/ppu.ml
+++ lib/ppu.ml
@@ -5,12 +5,13 @@ module Registers = struct
     mutable control : int;
     mutable mask : int;
     mutable status : int;
+    mutable oam : int;
     mutable scroll : int;
     mutable address : int;
     mutable data : int;
   }
   let make () = {
-    status = 0; address = 0; control = 0; mask = 0; scroll = 0; data = 0;
+    status = 0; address = 0; control = 0; mask = 0; scroll = 0; data = 0; oam = 0;
   }
 end
 
@@ -19,15 +20,35 @@ type ppu = {
   mutable scanline : int;
   mutable frame : int;
   mutable vram_rw_high : bool;
+  mutable register : int;
+  mutable vblank : bool;
+  mutable nmi : bool;
   registers: Registers.registers;
   vram: int array;
+  oam: int array;
+  rom : Cartridge.rom;
 }
 
-let make () = {
+let make ~rom = {
   cycle = 0; scanline = 0; frame = 0; vram_rw_high = true;
   registers = Registers.make ();
+  register = 0; vblank = true; nmi = false;
   vram = Array.create ~len:0x4000 0;
+  oam = Array.create ~len:0x100 0;
+  rom;
 }
+
+let load ppu address =
+  if address < 0x2000 then print_endline "prout";
+
+  if address < 0x2000 then
+    ppu.rom.chr.(address) (* CHR *)
+  else if address < 0x3F00 then
+    0xDEA1 (* Name Tables *)
+  else if address < 0x4000 then
+    0xDEA2 (* Palettes *)
+  else
+    failwith @@ sprintf "Trying to read PPU VRAM @ %04X" address
 
 let address_increment ppu =
   match ppu.registers.control land 0x04 with
@@ -38,43 +59,70 @@ let read_register ppu = function
   | 0x2000 -> ppu.registers.control
   | 0x2001 -> ppu.registers.mask
   | 0x2002 ->
-    let status = ppu.registers.status in
-    ppu.registers.status <- status lxor 0x80;
-    status
+    ppu.registers.status <- ppu.registers.status lxor 0x80;
+    ppu.vram_rw_high <- true;
+    let result = ppu.register land 0x1F lor (if ppu.vblank then 0x80 else 0) in
+    ppu.vblank <- false;
+    result
+  | 0x2004 -> ppu.oam.(ppu.registers.oam)
   | 0x2005 -> ppu.registers.scroll
   | 0x2006 -> ppu.registers.address
-  (* | 0x2007 -> ppu.registers.data *)
-  | _ as r -> failwith @@ sprintf "Cannot read PPU @ %04X" r
+  | 0x2007 ->
+    let address = ppu.registers.address in
+    (* printf "SUPBRAH %04X %02X\n" address (load ppu address); *)
+    ppu.registers.address <- ppu.registers.address + address_increment ppu;
+    (* ppu.vram.(address) *)
+    load ppu address
+  | _ as r -> failwith @@ sprintf "Cannot read PPU Register @ %04X" r
 
-let write_register ppu register value = match register with
+let write_register ppu register value =
+  ppu.register <- value;
+  match register with
   | 0x2000 -> ppu.registers.control <- value
   | 0x2001 -> ppu.registers.mask <- value
+  | 0x2003 -> ppu.registers.oam <- value
   | 0x2005 -> ppu.registers.scroll <- value
   | 0x2006 ->
+    (* printf "SUPBRAH %04X %02X\n" address (load ppu address); *)
     if ppu.vram_rw_high then (
-      ppu.registers.address <- (ppu.registers.address land 0x80FF) lor ((value land 0x3F) lsl 8);
+      ppu.registers.address <- ((ppu.registers.address land 0xFF) lor (value lsl 8) land 0x3FFF);
+      (* printf "SUPBRAH hi %04X\n" ppu.registers.address; *)
       ppu.vram_rw_high <- false
     ) else (
       ppu.registers.address <- (ppu.registers.address land 0xFF00) lor value;
+      (* printf "SUPBRAH lo %04X\n" ppu.registers.address; *)
       ppu.vram_rw_high <- true
     )
   | 0x2007 ->
     let address = ppu.registers.address in
-    ppu.vram.(address land 0x3FFF) <- value;
-    ppu.registers.address <- ppu.registers.address + address_increment ppu
-  | _ as r -> failwith @@ sprintf "Cannot write PPU @ %04X" r
+    ppu.vram.(address) <- value;
+    (* printf "WRITEBRAH %04X %02X\n" address value; *)
+    ppu.registers.address <- (ppu.registers.address + address_increment ppu) land 0x3FFF
+  | _ as r -> failwith @@ sprintf "Cannot write PPU Register @ %04X" r
 
-(* module type PpuRegister = sig
-  val value : int ref
-end
+let step ppu =
+  ppu.nmi <- false;
+  let scanline = ppu.scanline in
+  (* if scanline >= 0 && scanline < 240 then *)
+    (* print_endline "making pixels"; DRAW STUFF *)
 
-module MakeRegister(M: PpuRegister) : PpuRegister = struct
-  let value = M.value
-end
+  (* if scanline = 241 && ppu.cycle = 1 then ( *)
+  if scanline = 241 then (
+    ppu.vblank <- true;
+    if ppu.registers.control land 0x80 > 0 then
+      ppu.nmi <- true
+    (* print_endline "=============== VBLANK ==============" *)
+  );
 
-module Ctrl = MakeRegister(struct
-  let value = ref 0
-  let vram_increment = match !value with
-  | 0x04 -> 1
-  | _ -> 32
-end) *)
+  ppu.cycle <- ppu.cycle + 1;
+
+  if ppu.cycle >= 341 then (
+    ppu.cycle <- 0;
+    ppu.scanline <- ppu.scanline + 1);
+
+  if ppu.scanline = 262 then (
+    ppu.scanline <- 0;
+    ppu.frame <- ppu.frame + 1;
+    (* Render stuff here *)
+    ppu.vblank <- false;
+  )
