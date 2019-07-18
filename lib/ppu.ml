c7cdeb5da308afa26313cc558ebf67708b535f92
Xavier Perez <xperez@fastly.com>
2019-07-17 23:41:26 -0700

    Stuff shows up on screen!

--- lib/ppu.ml
+++ lib/ppu.ml
@@ -15,15 +15,23 @@ module Registers = struct
   }
 end
 
+type ppu_result = {
+  new_frame : bool
+}
+
 type ppu = {
   mutable cycle : int;
   mutable scanline : int;
-  mutable frame : int;
+  mutable frames : int;
   mutable vram_rw_high : bool;
   mutable register : int;
   mutable vblank : bool;
   mutable nmi : bool;
+  mutable new_frame : bool;
+  (* frame_content : int array; *)
+  frame_content : (int, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t;
   registers: Registers.registers;
+  palettes : int array;
   vram: int array;
   oam: int array;
   nametables : int array;
@@ -31,9 +39,12 @@ type ppu = {
 }
 
 let make ~rom = {
-  cycle = 0; scanline = 0; frame = 0; vram_rw_high = true;
+  cycle = 0; scanline = 0; frames = 0; vram_rw_high = true;
   registers = Registers.make ();
-  register = 0; vblank = true; nmi = false;
+  register = 0; vblank = true; nmi = false; new_frame = false;
+  (* frame_content = Array.create ~len:(256 * 240 * 3) 0xFF00FF; *)
+  frame_content = Bigarray.Array1.create Bigarray.int8_unsigned Bigarray.c_layout (256 * 240 * 3);
+  palettes = Array.create ~len:32 0;
   vram = Array.create ~len:0x4000 0;
   oam = Array.create ~len:0x100 0;
   nametables = Array.create ~len:0x800 0;
@@ -46,7 +57,7 @@ let load ppu address =
   else if address < 0x3F00 then
     ppu.nametables.(address land 0x07FF)
   else if address < 0x4000 then
-    0xDEA2 (* Palettes *)
+    ppu.palettes.(address land 0x1F)
   else
     failwith @@ sprintf "Trying to read PPU VRAM @ %04X" address
 
@@ -56,7 +67,7 @@ let store ppu address value =
   else if address < 0x3F00 then
     ppu.nametables.(address land 0x07FF) <- value
   else if address < 0x4000 then
-    ()
+    ppu.palettes.(address land 0x1F) <- value
   else
     failwith @@ sprintf "Trying to write PPU VRAM @ %04X" address
 
@@ -106,29 +117,76 @@ let write_register ppu register value =
     ppu.registers.address <- ((ppu.registers.address + address_increment ppu) land 0xFFFF) land 0x3FFF
   | _ as r -> failwith @@ sprintf "Cannot write PPU Register @ %04X" r
 
+let get_pixel ppu x offset =
+  let p0 = load ppu offset in
+  let p1 = load ppu (offset + 8) in
+  let bit0 = (p0 lsr (7 - ((x % 8) land 0xFF))) land 1 in
+  let bit1 = (p1 lsr (7 - ((x % 8) land 0xFF))) land 1 in
+  (bit1 lsl 1) lor bit0
+
+let get_background_pixel ppu x =
+  let x_offset = x / 8 in
+  let y_offset = ppu.scanline / 8 in
+  let x2 = x % 8 in
+  let y2 = (ppu.scanline % 8) land 0xFF in
+
+  let tile_address = 0x2000 + 32 * y_offset + x_offset in
+  let tile = load ppu tile_address in
+  let offset = (((tile lsl 4) + y2) land 0xFFFF) + 0x1000 in
+  (* let offset = (((tile lsl 4) + y2) land 0xFFFF) in *)
+  let pixel = get_pixel ppu x2 offset in
+  let c = (pixel lsl 6) land 0xFFFFFFFF in
+  (c lsl 8) lor (c lsl 16) lor (c lsl 24)
+
+  (* let block = y_offset / 4 * 8 + x_offset / 4 in
+  let attributes = load ppu (0x23C0 + block) in
+  let left = x_offset % 4 < 2 in
+  let top = y_offset % 4 < 2 in
+
+  let c = match (left, top) with
+  | (true, true)   -> attributes
+  | (false, true)  -> attributes lsr 2
+  | (true, false)  -> attributes lsr 4
+  | (false, false) -> attributes lsr 6
+  in
+
+  let color = ((c land 0x3) lsl 2) lor pixel in
+  let palette_address = 0x3F00 + (color land 0xFFFF) in
+  let palette = load ppu (palette_address & 0x3F) in *)
+
+
+let set_pixel ppu x y color =
+  ppu.frame_content.{(y * 256 + x) * 3 + 0} <- color lsr 24;
+  ppu.frame_content.{(y * 256 + x) * 3 + 1} <- color lsr 16;
+  ppu.frame_content.{(y * 256 + x) * 3 + 2} <- color lsr 8
+
 let step ppu =
   ppu.nmi <- false;
-  let scanline = ppu.scanline in
-  (* if scanline >= 0 && scanline < 240 then *)
-    (* print_endline "making pixels"; DRAW STUFF *)
 
-  (* if scanline = 241 && ppu.cycle = 1 then ( *)
-  if scanline = 241 then (
+  let scanline = ppu.scanline in
+  if scanline >= 0 && scanline < 240 then (
+    for x = 0 to 255 do
+      let colour = get_background_pixel ppu x in
+      set_pixel ppu x scanline colour
+    done;
+  );
+
+  if scanline = 241 && ppu.cycle = 1 then (
     ppu.vblank <- true;
-    if ppu.registers.control land 0x80 > 0 then
+    if ppu.registers.control land 0xFF > 0 then
       ppu.nmi <- true
-    (* print_endline "=============== VBLANK ==============" *)
   );
 
   ppu.cycle <- ppu.cycle + 1;
 
   if ppu.cycle >= 341 then (
     ppu.cycle <- 0;
-    ppu.scanline <- ppu.scanline + 1);
+    ppu.scanline <- ppu.scanline + 1
+  );
 
-  if ppu.scanline = 262 then (
+  if ppu.scanline >= 262 then (
     ppu.scanline <- 0;
-    ppu.frame <- ppu.frame + 1;
-    (* Render stuff here *)
+    ppu.frames <- ppu.frames + 1;
+    ppu.new_frame <- true;
     ppu.vblank <- false;
-  )
+  );
