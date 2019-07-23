c6b4e3b750ee76e23cb0404fe28da1be1e207fdb
Xavier Perez <duosrx@gmail.com>
2019-07-22 19:49:04 -0700

    Begin work on mappers

--- lib/nes.ml
+++ lib/nes.ml
@@ -1,3 +1,4 @@
+open Core
 module Cartridge = Cartridge
 module Cpu = Cpu
 module Input = Input
@@ -8,12 +9,19 @@ type t = {
   cpu : Cpu.cpu;
   ppu : Ppu.ppu;
   rom : Cartridge.rom;
-  (* TODO: input mapper apu *)
+  ram : int array;
+}
+
+type memmap = {
+  ppu : Ppu.ppu;
+  mapper : Mapper.t
 }
 
 let make ?(nestest=false) ?(tracing=false) rom =
   let ppu = Ppu.make ~rom in
-  let cpu = Cpu.make ~rom ~ppu ~nestest ~tracing in
+  let ram = Array.create ~len:0x800 0 in
+  let mapper = Mapper.mapper_from_rom rom in
+  let cpu = Cpu.make ~rom ~ppu ~nestest ~tracing ~mapper in
   {
-    ppu; rom; cpu
+    ppu; rom; cpu; ram;
   }
